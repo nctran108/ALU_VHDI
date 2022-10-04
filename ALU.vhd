ff0ff856251198ca9b26e4f6230c2d1af051f057
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2022-10-04 16:07:33 -0400

    update

--- ALU.vhd
+++ ALU.vhd
@@ -8,56 +8,77 @@ entity ALU is
           Zero          : out   std_logic;
           Overflow      : out   std_logic;
           Carryout      : out   std_logic;
-          Result        : out   std_logic_vector(31 downto 0)
+          Result        : inout   std_logic_vector(31 downto 0)
     );
 end ALU;
 
 architecture Behavioral of ALU is
+signal sumResult, subResult : std_logic_vector(31 downto 0);
+signal c_in, c_out, is_zero,c_in1, c_out1,is_overflow: std_logic := '0';
 
-signal c_in, c_out : std_logic := '0';
-
-component N_bit_adder
-    generic(N: integer := 32);
-    Port (A,B : in std_logic_vector (N-1 downto 0);
-          C_in: in std_logic;
-          C_out: out std_logic;
-          S: out std_logic_vector (N-1 downto 0));
-    end component;
 begin   
+c_in1 <= '1';
+adder: entity work.N_bit_adder(Behavioral)
+    generic map(32)
+    port map (A => busA,
+              B => busB,
+              C_in => c_in,
+              C_out => c_out,
+              Overflow => is_overflow,
+              S => sumResult);
 
-addition: if ALUctr = "0000" generate
-    adder: N_bit_adder port map (busA,busB,c_in,c_out,Result);
-    end generate addition;
+subtracter: entity work.N_bit_adder(Behavioral)
+    generic map(32)
+    port map (A => busA,
+              B => busB,
+              C_in => c_in1,
+              C_out => c_out1,
+              Overflow => is_overflow,
+              S => subResult);
 
-subtraction: if ALUctr = "0001" generate
--- component
-    end generate subtraction;
+mux: process(ALUctr,busA,busB)
+begin
+    case ALUctr is
+    when "0000" => --addition
+        Result <= sumResult;
+        Zero <= is_zero;
+        Carryout <= is_overflow;
+        Overflow <= '0';
+    when "0001" => --subtraction
+        Result <= subResult;
+        Zero <= is_zero;
+        Carryout <= c_out1;
+        Overflow <= is_overflow;
+    when "0010" => --Bitwise AND
+        Result <= busA and busB;
+        Zero <= is_zero;
+        Carryout <= '0';
+        Overflow <= '0';
+    when "0011" => --Bitwise OR
+        Result <= busA or busB;
+        Zero <= is_zero;
+        Carryout <= '0';
+        Overflow <= '0';
+    when "0100" => --Logical left shift
+    when "0101" => --Logical right shift
+    when "0110" => --Arithmetic left shift
+    when "0111" => --Arithmetic right shift
+    when "1000" => --Multiplier
+    when others => --other cases
+        Result <= (others => '0');   
+        Zero <= '0';
+        Carryout <= '0';
+        Overflow <= '0';
+    end case;
+    end process mux;
 
-Bitwise_AND: if ALUctr = "0010" generate
--- component
-    end generate Bitwise_AND;
-
-Bitwise_OR: if ALUctr = "0011" generate
--- component
-    end generate Bitwise_OR;
-
-Logical_left_shift: if ALUctr = "0100" generate
--- component
-    end generate Logical_left_shift;
-
-Logical_right_shift: if ALUctr = "0101" generate
--- component
-    end generate Logical_right_shift;
-
-Arithmetic_left_shift: if ALUctr = "0110" generate
--- component
-    end generate Arithmetic_left_shift;
-
-Arithmetic_right_shift: if ALUctr = "0111" generate
--- component
-    end generate Arithmetic_right_shift;
-
-Multiplier: if ALUctr = "1000" generate
--- component
-    end generate Multiplier;
+count: process(Result)
+variable count: integer := 0;
+begin
+    if Result = x"00000000" then
+        is_zero <= '1';
+    else
+        is_zero <= '0';
+    end if;
+    end process count;
 end Behavioral;
--- N_bit_adder.vhd
+++ N_bit_adder.vhd
@@ -8,23 +8,25 @@ entity N_bit_adder is
     Port (A,B : in std_logic_vector (N-1 downto 0);
           C_in: in std_logic;
           C_out: out std_logic;
+          Overflow: out std_logic;
           S: out std_logic_vector (N-1 downto 0));
 end N_bit_adder;
 
 architecture Behavioral of N_bit_adder is
-constant MAX_DELAY: time := 14 ns;
+--constant MAX_DELAY: time := 14 ns;
 signal c: std_logic_vector (N-1 downto 0);
 begin
 Add : process(A,B,C_in,c)
     begin
-        S(0) <= A(0) XOR C_in after MAX_DELAY;
-        c(0) <= (A(0) AND C_in) OR (B(0) AND C_in) OR (A(0) AND B(0)) after MAX_DELAY;
+        S(0) <= (A(0) XOR B(0)) XOR C_in;
+        c(0) <= (A(0) AND (B(0)or C_in)) OR (B(0) AND C_in);
         
         for i in 1 to (S'length - 1) loop
-            S(i) <= A(i) XOR B(i) XOR c(i-1) after MAX_DELAY;
-            c(i) <= (A(i) AND c(i-1)) OR (B(i) AND c(i-1)) OR (A(i) AND B(i)) after MAX_DELAY;
+            S(i) <= (A(i) XOR B(i)) XOR c(i-1);
+            c(i) <= (A(i) AND (B(i)or C(i-1))) OR (B(i) AND C(i-1));
         end loop;
         C_out <= c(N-1);
+        Overflow <= c(N-1) xor c(N-2);
     end process Add;
     
 end Behavioral;
